2c06c48753788765d8c8d582a5b70f7e9238faf9
JomMagz <jershom95@gmail.com>
2024-11-08 20:37:20 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,74 @@
+// console.log("Hello World")
+// console.log("welcome to web dev 101")
+
+// let x
+// let y
+
+// function funcAdd(x, y) {
+//     return x + y
+// }
+// function funcSub(x, y) {
+//     return x - y
+// }
+// function funcMult(x, y) {
+//     return x * y
+// }
+// function funcDivide(x, y) {
+//     return x / y
+// }
+// function funcExpo(x, y) {
+//     return x ** y
+// }
+// function funcModul(x, y) {
+//     return x % y
+// }
+
+// console.log(funcAdd(123, 456))
+// console.log(funcSub(123, 456))
+// console.log(funcMult(123, 456))
+// console.log(funcDivide(123, 456))
+// console.log(funcExpo(9, 3))
+// console.log(funcModul(123, 456))
+
+// const age = 18
+
+// if (age < 18){
+//     console.log("YOUNG!")
+// } else if (age === 18){
+//     console.log("TEEN")
+// } else {
+//     console.log("ADULT")
+// }
+
+
+// for(let x = 0; x <= 100; x++){
+//     if (x % 2 != 0){
+//         console.log(x)
+//     }
+// }
+
+// let x=0
+
+// while(x <= 10){
+//     console.log(x)
+//     x++
+// }
+
+// const body = document.body
+// const div = document.createElement("div")
+// body.append(div)
+
+// div.innerHTML = "<strong>Hello World</strong>"
+
+// const body = document.body
+// const div = document.querySelector("div")
+// const spanHi = document.querySelector("#hi")
+// const spanlow = document.querySelector("#low")
+
+console.log(spanHi.getAttribute("title"))
+spanHi.setAttribute("id", "change-id")
+spanHi.removeAttribute("id")
+
+// console.log(spanHi.dataset)
+// spanHi.dataset.newName = "Created Using JS"
+
